762c36d599b62558844409a4c80ef45e398415c3
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-07-02 11:52:14 +0900

    add csv download feature

--- rakuten-card/src/index.ts
+++ rakuten-card/src/index.ts
@@ -3,17 +3,27 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const LOGIN_URL = 'https://www.rakuten-card.co.jp/e-navi/index.xhtml';
+const TOP_URL = 'https://www.rakuten-card.co.jp/e-navi/members/index.xhtml';
 const DOWNLOAD_LIST_URL =
 	'https://www.rakuten-card.co.jp/e-navi/members/statement/download-list.xhtml';
+
+const DOWNLOAD_CSV_URL =
+	'https://www.rakuten-card.co.jp/e-navi/members/statement/index.xhtml?tabNo=1';
 
 const sleep = (time: number): Promise<void> =>
 	new Promise(resolve => setTimeout(resolve, time * 1000));
 
 const INTERVAL = 10;
 
-const clickLatestUrl = async (page: Page): Promise<void> => {
+const clickLatestPdfUrl = async (page: Page): Promise<void> => {
 	await page.click(
 		'a[href="/e-navi/members/statement/download-list.xhtml?downloadAsPdf=0"]'
+	);
+};
+
+const clickLatestCsvUrl = async (page: Page): Promise<void> => {
+	await page.click(
+		'a[href="/e-navi/members/statement/index.xhtml?downloadAsCsv=1"]'
 	);
 };
 
@@ -21,7 +31,7 @@ const getCardCount = async (page: Page) => {
 	return await page.evaluate(() => {
 		const i = (
 			document.querySelector(
-				'select[id="cardChangeForm:card"]'
+				'select[id="cardChangeForm:cardtype"]'
 			) as HTMLSelectElement
 		).options.length;
 		console.log(i);
@@ -41,29 +51,38 @@ const getCardCount = async (page: Page) => {
 	await page.type('input[id="p"]', process.env.PASSWORD);
 	await page.click('input[id="loginButton"]');
 	await sleep(INTERVAL);
-	await page.goto(DOWNLOAD_LIST_URL);
+	await page.goto(TOP_URL);
 	await sleep(INTERVAL);
-	clickLatestUrl(page);
-	await sleep(INTERVAL);
-
 	const cardCount = await getCardCount(page);
-	for (let index = 1; index < cardCount; index++) {
-		await page.evaluate(index => {
-			const e: HTMLSelectElement | null = document.querySelector(
-				'select[id="cardChangeForm:card"]'
-			);
-			if (e) {
-				e.selectedIndex = index;
-			}
+	for (let index = 0; index < cardCount; index++) {
+		if (index != 0) {
+			await page.goto(TOP_URL);
+			await sleep(INTERVAL);
+			await page.evaluate(index => {
+				const e: HTMLSelectElement | null = document.querySelector(
+					'select[id="cardChangeForm:cardtype"]'
+				);
+				if (e) {
+					e.selectedIndex = index;
+				}
 
-			const form: HTMLElement | null =
-				document.getElementById('cardChangeForm');
-			if (form) {
-				(form as HTMLFormElement).submit();
-			}
-		}, index);
+				const form: HTMLElement | null =
+					document.getElementById('cardChangeForm');
+				if (form) {
+					(form as HTMLFormElement).submit();
+				}
+			}, index);
+			await sleep(INTERVAL);
+		}
+
+		await page.goto(DOWNLOAD_LIST_URL);
 		await sleep(INTERVAL);
-		clickLatestUrl(page);
+		await clickLatestPdfUrl(page);
+		await sleep(INTERVAL);
+
+		await page.goto(DOWNLOAD_CSV_URL);
+		await sleep(INTERVAL);
+		await clickLatestCsvUrl(page);
 		await sleep(INTERVAL);
 	}
 	await sleep(INTERVAL);
