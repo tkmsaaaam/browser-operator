adda7847502ed78107250bb6e22d9dd62153d347
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-10-10 08:10:03 +0900

    refactor

--- rakuten-sec/src/index.ts
+++ rakuten-sec/src/index.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer-core';
+import puppeteer, { Page } from 'puppeteer-core';
 import { getPassword } from './authentication';
 
 type Index = {
@@ -62,10 +62,20 @@ const MARKET_URL = 'https://member.rakuten-sec.co.jp/app/market_top.do;';
 const FAVORITE_LIST =
 	'https://member.rakuten-sec.co.jp/app/info_jp_prc_reg_lst.do;';
 
-const INTERVAL = 10;
+const INTERVAL = 1;
 
 const sleep = (time: number): Promise<void> =>
 	new Promise(resolve => setTimeout(resolve, time * 1000));
+
+const getBvSessionId = async (page: Page): Promise<string> => {
+	for (let index = 0; index < 10; index++) {
+		if (page.url().includes('BV_SessionID=')) {
+			return page.url().split(';')[1].split('?')[0];
+		}
+		await sleep(INTERVAL);
+	}
+	return page.url().split(';')[1].split('?')[0];
+};
 
 (async () => {
 	const username = process.env.USERNAME;
@@ -91,11 +101,10 @@ const sleep = (time: number): Promise<void> =>
 	await page.type('input[id="form-login-id"]', username);
 	await page.type('input[id="form-login-pass"]', password);
 	await page.click('button[id="login-btn"]');
-	await sleep(INTERVAL);
-	const bvSessionId = page.url().split(';')[1].split('?')[0];
+
+	const bvSessionId = await getBvSessionId(page);
 
 	await page.goto(MARKET_URL + bvSessionId + '?eventType=init');
-	await sleep(INTERVAL * 2);
 
 	const initializedIndex: Index = {
 		current: 0,
@@ -110,52 +119,63 @@ const sleep = (time: number): Promise<void> =>
 	};
 
 	const market: Market = await page.evaluate((resultMarcket: Market) => {
-		resultMarcket.yenPerDollar.current = Number(
-			(
-				document.querySelector(
-					'td[id="auto_update_market_index_exchange_XXX31_ask"]'
-				) as HTMLTableElement
-			).innerText
-		);
-		resultMarcket.yenPerDollar.diffAmount = Number(
-			(
-				document.querySelector(
-					'td[id="auto_update_market_index_exchange_XXX31_net_change"]'
-				) as HTMLTableElement
-			).innerText
-		);
-		resultMarcket.yenPerDollar.diffRate = Number(
-			(
-				document.querySelector(
-					'td[id="auto_update_market_index_exchange_XXX31_bid_percent_change"]'
-				) as HTMLTableElement
-			).innerText.replace('%', '')
-		);
-		resultMarcket.yenPerDollar.dateTime = (
-			document.querySelector(
-				'td[id="auto_update_market_index_exchange_XXX31_now_date"]'
-			) as HTMLTableElement
-		).innerText;
+		for (let index = 0; index < 20; index++) {
+			const target = document.querySelector(
+				'td[id="auto_update_market_index_exchange_XXX31_ask"]'
+			);
+			if (!target) {
+				sleep(INTERVAL);
+				continue;
+			}
 
-		resultMarcket.bonds10.current = Number(
-			(
+			resultMarcket.yenPerDollar.current = Number(
+				(
+					document.querySelector(
+						'td[id="auto_update_market_index_exchange_XXX31_ask"]'
+					) as HTMLTableElement
+				).innerText
+			);
+			resultMarcket.yenPerDollar.diffAmount = Number(
+				(
+					document.querySelector(
+						'td[id="auto_update_market_index_exchange_XXX31_net_change"]'
+					) as HTMLTableElement
+				).innerText
+			);
+			resultMarcket.yenPerDollar.diffRate = Number(
+				(
+					document.querySelector(
+						'td[id="auto_update_market_index_exchange_XXX31_bid_percent_change"]'
+					) as HTMLTableElement
+				).innerText.replace('%', '')
+			);
+			resultMarcket.yenPerDollar.dateTime = (
 				document.querySelector(
-					'td[id="auto_update_market_index_bond_BD005_annualized_yield"]'
+					'td[id="auto_update_market_index_exchange_XXX31_now_date"]'
 				) as HTMLTableElement
-			).innerText
-		);
-		resultMarcket.bonds10.diffRate = Number(
-			(
+			).innerText;
+
+			resultMarcket.bonds10.current = Number(
+				(
+					document.querySelector(
+						'td[id="auto_update_market_index_bond_BD005_annualized_yield"]'
+					) as HTMLTableElement
+				).innerText
+			);
+			resultMarcket.bonds10.diffRate = Number(
+				(
+					document.querySelector(
+						'td[id="auto_update_market_index_bond_BD005_net_change"]'
+					) as HTMLTableElement
+				).innerText.replace('%', '')
+			);
+			resultMarcket.bonds10.dateTime = (
 				document.querySelector(
-					'td[id="auto_update_market_index_bond_BD005_net_change"]'
+					'td[id="auto_update_market_index_bond_BD005_now_date"]'
 				) as HTMLTableElement
-			).innerText.replace('%', '')
-		);
-		resultMarcket.bonds10.dateTime = (
-			document.querySelector(
-				'td[id="auto_update_market_index_bond_BD005_now_date"]'
-			) as HTMLTableElement
-		).innerText;
+			).innerText;
+			break;
+		}
 		return resultMarcket;
 	}, initializedMarket);
 
@@ -165,203 +185,219 @@ const sleep = (time: number): Promise<void> =>
 			'?eventType=directInit&l-id=mem_pc_top_all-possess-lst&gmn=H&smn=01&lmn=&fmn='
 	);
 
-	await sleep(INTERVAL);
-
 	const initializedAsset: Asset = {
 		total: { amount: 0, diff: 0, diffRate: 0 },
 		possessList: [],
 	};
 
 	const asset: Asset = await page.evaluate((asset: Asset) => {
-		const amount = Number(
-			(
-				document.querySelector(
-					'td[class="R1 B3 f105p"] span[class="fb"]'
-				) as HTMLSpanElement
-			).innerText.replace(/円|,/g, '')
-		);
-		asset.total.amount = amount;
-		const diff = Number(
-			(
-				document.querySelector('span[class="PLY"]') as HTMLSpanElement
-			).innerText.replace(/円|,/g, '')
-		);
-		asset.total.diff = diff;
-		asset.total.diffRate = (diff / amount) * 100;
-
-		const tableProcessData = document.getElementById('table_possess_data');
-		if (!tableProcessData) return asset;
-		const possessList = tableProcessData.getElementsByTagName('tr');
-		for (let index = 3; index < possessList.length; index++) {
-			const dataRaw = possessList[index].getElementsByTagName('td');
-			const securityType = dataRaw[0].innerText;
-			if (securityType == '米国株式') {
-				const currentPrice = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/USD|,/g, '')
-				);
-				const diff = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[1]
-						.innerText.replace(/USD|,/g, '')
-				);
-				const totalAmount = Number(
-					dataRaw[7]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/円|,/g, '')
-				);
-				const profit = Number(
-					dataRaw[7]
-						.getElementsByTagName('div')[2]
-						.innerText.replace(/円|,|\n|\t/g, '')
-				);
-				let profitRate = 0;
-				if (profit >= 0) {
-					profitRate = profit / totalAmount;
-				} else {
-					profitRate = profit / (totalAmount - profit);
-				}
-
-				const possess: Possess = {
-					securityType: securityType,
-					name: dataRaw[1].innerText,
-					accountType: dataRaw[3].innerText,
-					buyPrice: Number(dataRaw[5].innerText.replace(/USD|,/g, '')),
-					currentPrice: currentPrice,
-					diff: diff,
-					diffRate: (diff / currentPrice) * 100,
-					count: Number(dataRaw[4].innerText.replace(/株|,/g, '')),
-					totalAmount: totalAmount,
-					profit: profit,
-					profitRate: profitRate * 100,
-				};
-				asset.possessList.push(possess);
-			} else if (securityType == '投資信託') {
-				const currentPrice = Number(
-					dataRaw[5]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/円|,/g, '')
-				);
-				const diff = Number(
-					dataRaw[5]
-						.getElementsByTagName('div')[1]
-						.innerText.replace(/円|,/g, '')
-				);
-				const totalAmount = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/円|,/g, '')
-				);
-				const profit = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[2]
-						.innerText.replace(/円|,|\n|\t/g, '')
-				);
-				let profitRate = 0;
-				if (profit >= 0) {
-					profitRate = profit / totalAmount;
-				} else {
-					profitRate = profit / (totalAmount - profit);
-				}
-				const possess: Possess = {
-					securityType: securityType,
-					name: dataRaw[1].innerText,
-					accountType: dataRaw[2].innerText.replace('\n', ''),
-					buyPrice: Number(dataRaw[4].innerText.replace(/円|,/g, '')),
-					currentPrice: currentPrice,
-					diff: diff,
-					diffRate: (diff / currentPrice) * 100,
-					count: Number(dataRaw[3].innerText.replace(/口|,/g, '')),
-					totalAmount: totalAmount,
-					profit: profit,
-					profitRate: profitRate * 100,
-				};
-				asset.possessList.push(possess);
-			} else if (securityType == '外貨預り金') {
-				const currentPrice = Number(
-					dataRaw[5]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/円\/USD|,/g, '')
-				);
-				const diff = Number(
-					dataRaw[5]
-						.getElementsByTagName('div')[1]
-						.innerText.replace(/円|,/g, '')
-				);
-				const totalAmount = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[0]
-						.innerText.replace(/円|,/g, '')
-				);
-				const profit = Number(
-					dataRaw[6]
-						.getElementsByTagName('div')[2]
-						.innerText.replace(/円|,|\n|\t/g, '')
-				);
-				let profitRate = 0;
-				if (profit >= 0) {
-					profitRate = profit / totalAmount;
-				} else {
-					profitRate = profit / (totalAmount - profit);
-				}
-				const possess: Possess = {
-					securityType: securityType,
-					name: dataRaw[1].innerText,
-					accountType: dataRaw[2].innerText.replace('\n', ''),
-					buyPrice: Number(dataRaw[4].innerText.replace(/円\/USD|,/g, '')),
-					currentPrice: currentPrice,
-					diff: diff,
-					diffRate: (diff / currentPrice) * 100,
-					count: Number(dataRaw[3].innerText.replace(/USD|,/g, '')),
-					totalAmount: totalAmount,
-					profit: profit,
-					profitRate: profitRate * 100,
-				};
-				asset.possessList.push(possess);
+		for (let index = 0; index < 10; index++) {
+			const target = document.querySelector(
+				'td[class="R1 B3 f105p"] span[class="fb"]'
+			);
+			if (!target) {
+				sleep(INTERVAL);
+				continue;
 			}
-		}
 
+			const amount = Number(
+				(
+					document.querySelector(
+						'td[class="R1 B3 f105p"] span[class="fb"]'
+					) as HTMLSpanElement
+				).innerText.replace(/円|,/g, '')
+			);
+			asset.total.amount = amount;
+			const diff = Number(
+				(
+					document.querySelector('span[class="PLY"]') as HTMLSpanElement
+				).innerText.replace(/円|,/g, '')
+			);
+			asset.total.diff = diff;
+			asset.total.diffRate = (diff / amount) * 100;
+
+			const tableProcessData = document.getElementById('table_possess_data');
+			if (!tableProcessData) return asset;
+			const possessList = tableProcessData.getElementsByTagName('tr');
+			for (let index = 3; index < possessList.length; index++) {
+				const dataRaw = possessList[index].getElementsByTagName('td');
+				const securityType = dataRaw[0].innerText;
+				if (securityType == '米国株式') {
+					const currentPrice = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/USD|,/g, '')
+					);
+					const diff = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[1]
+							.innerText.replace(/USD|,/g, '')
+					);
+					const totalAmount = Number(
+						dataRaw[7]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/円|,/g, '')
+					);
+					const profit = Number(
+						dataRaw[7]
+							.getElementsByTagName('div')[2]
+							.innerText.replace(/円|,|\n|\t/g, '')
+					);
+					let profitRate = 0;
+					if (profit >= 0) {
+						profitRate = profit / totalAmount;
+					} else {
+						profitRate = profit / (totalAmount - profit);
+					}
+
+					const possess: Possess = {
+						securityType: securityType,
+						name: dataRaw[1].innerText,
+						accountType: dataRaw[3].innerText,
+						buyPrice: Number(dataRaw[5].innerText.replace(/USD|,/g, '')),
+						currentPrice: currentPrice,
+						diff: diff,
+						diffRate: (diff / currentPrice) * 100,
+						count: Number(dataRaw[4].innerText.replace(/株|,/g, '')),
+						totalAmount: totalAmount,
+						profit: profit,
+						profitRate: profitRate * 100,
+					};
+					asset.possessList.push(possess);
+				} else if (securityType == '投資信託') {
+					const currentPrice = Number(
+						dataRaw[5]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/円|,/g, '')
+					);
+					const diff = Number(
+						dataRaw[5]
+							.getElementsByTagName('div')[1]
+							.innerText.replace(/円|,/g, '')
+					);
+					const totalAmount = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/円|,/g, '')
+					);
+					const profit = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[2]
+							.innerText.replace(/円|,|\n|\t/g, '')
+					);
+					let profitRate = 0;
+					if (profit >= 0) {
+						profitRate = profit / totalAmount;
+					} else {
+						profitRate = profit / (totalAmount - profit);
+					}
+					const possess: Possess = {
+						securityType: securityType,
+						name: dataRaw[1].innerText,
+						accountType: dataRaw[2].innerText.replace('\n', ''),
+						buyPrice: Number(dataRaw[4].innerText.replace(/円|,/g, '')),
+						currentPrice: currentPrice,
+						diff: diff,
+						diffRate: (diff / currentPrice) * 100,
+						count: Number(dataRaw[3].innerText.replace(/口|,/g, '')),
+						totalAmount: totalAmount,
+						profit: profit,
+						profitRate: profitRate * 100,
+					};
+					asset.possessList.push(possess);
+				} else if (securityType == '外貨預り金') {
+					const currentPrice = Number(
+						dataRaw[5]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/円\/USD|,/g, '')
+					);
+					const diff = Number(
+						dataRaw[5]
+							.getElementsByTagName('div')[1]
+							.innerText.replace(/円|,/g, '')
+					);
+					const totalAmount = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[0]
+							.innerText.replace(/円|,/g, '')
+					);
+					const profit = Number(
+						dataRaw[6]
+							.getElementsByTagName('div')[2]
+							.innerText.replace(/円|,|\n|\t/g, '')
+					);
+					let profitRate = 0;
+					if (profit >= 0) {
+						profitRate = profit / totalAmount;
+					} else {
+						profitRate = profit / (totalAmount - profit);
+					}
+					const possess: Possess = {
+						securityType: securityType,
+						name: dataRaw[1].innerText,
+						accountType: dataRaw[2].innerText.replace('\n', ''),
+						buyPrice: Number(dataRaw[4].innerText.replace(/円\/USD|,/g, '')),
+						currentPrice: currentPrice,
+						diff: diff,
+						diffRate: (diff / currentPrice) * 100,
+						count: Number(dataRaw[3].innerText.replace(/USD|,/g, '')),
+						totalAmount: totalAmount,
+						profit: profit,
+						profitRate: profitRate * 100,
+					};
+					asset.possessList.push(possess);
+				}
+			}
+			break;
+		}
 		return asset;
 	}, initializedAsset);
 
 	await page.goto(FAVORITE_LIST + bvSessionId + '?eventType=init');
-	await sleep(INTERVAL * 2);
 	const favoriteList: Favorite[] = await page.evaluate((list: Favorite[]) => {
-		const trList = document
-			.getElementsByClassName('tbl-data-01')[0]
-			.getElementsByTagName('tbody')[0];
-		for (
-			let index = 1;
-			index < trList.getElementsByTagName('tr').length - 1;
-			index++
-		) {
-			const element = trList.getElementsByTagName('tr')[index];
-			const code = Number(element.getElementsByTagName('td')[1].innerText);
-			const name = element.getElementsByTagName('td')[3].innerText;
-			const market = element.getElementsByTagName('td')[6].innerText;
-			const current = Number(
-				element.getElementsByTagName('td')[7].innerText.replace(/↓|↑|,/g, '')
-			);
-			const updatedAt = element.getElementsByTagName('td')[8].innerText;
-			const diff = Number(element.getElementsByTagName('td')[9].innerText);
-			const diffRate = Number(
-				element.getElementsByTagName('td')[10].innerText.replace('%', '')
-			);
-			const transaction = Number(
-				element.getElementsByTagName('td')[11].innerText.replace(/株|,/g, '')
-			);
-			const favorite: Favorite = {
-				code: code,
-				name: name,
-				market: market,
-				current: current,
-				updatedAt: updatedAt,
-				diff: diff,
-				diffRate: diffRate,
-				transaction: transaction,
-			};
-			list.push(favorite);
+		for (let index = 0; index < 20; index++) {
+			const target = document.getElementsByClassName('tbl-data-01');
+			if (!target) {
+				sleep(INTERVAL);
+				continue;
+			}
+
+			const trList = document
+				.getElementsByClassName('tbl-data-01')[0]
+				.getElementsByTagName('tbody')[0];
+			for (
+				let index = 1;
+				index < trList.getElementsByTagName('tr').length - 1;
+				index++
+			) {
+				const element = trList.getElementsByTagName('tr')[index];
+				const code = Number(element.getElementsByTagName('td')[1].innerText);
+				const name = element.getElementsByTagName('td')[3].innerText;
+				const market = element.getElementsByTagName('td')[6].innerText;
+				const current = Number(
+					element.getElementsByTagName('td')[7].innerText.replace(/↓|↑|,/g, '')
+				);
+				const updatedAt = element.getElementsByTagName('td')[8].innerText;
+				const diff = Number(element.getElementsByTagName('td')[9].innerText);
+				const diffRate = Number(
+					element.getElementsByTagName('td')[10].innerText.replace('%', '')
+				);
+				const transaction = Number(
+					element.getElementsByTagName('td')[11].innerText.replace(/株|,/g, '')
+				);
+				const favorite: Favorite = {
+					code: code,
+					name: name,
+					market: market,
+					current: current,
+					updatedAt: updatedAt,
+					diff: diff,
+					diffRate: diffRate,
+					transaction: transaction,
+				};
+				list.push(favorite);
+			}
+			break;
 		}
 		return list;
 	}, []);
