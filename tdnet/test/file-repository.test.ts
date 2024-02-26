3c0e53f194e96465fd310d82e761c308330d0910
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-02-27 07:11:54 +0900

    update: add test/file-repository.test.ts

---
+++ tdnet/test/file-repository.test.ts
@@ -0,0 +1,26 @@
+import { describe, test, jest, expect } from '@jest/globals';
+import { getLastDateDiff } from '../src/file-repository';
+import fs from 'fs';
+import path from 'path';
+
+describe('getLastDateDiff', () => {
+	jest.mock('path', () => ({
+		resolve: () => 'test',
+	}));
+	// test('abnormalCase', () => {
+	// 	jest.mock('fs', () => ({
+	// 		existsSync: () => false,
+	// 		readFileSync: () => '2023-12-30',
+	// 	}));
+	// 	expect(getLastDateDiff()).toBe(30);
+	// });
+	test('normalCase', () => {
+		jest.mock('fs', () => ({
+			existsSync: () => true,
+			readFileSync: () => '2023-12-30',
+		}));
+		Date.now = jest.fn(() => 1704078000000);
+		Date.parse = jest.fn(() => 1703894400000);
+		expect(getLastDateDiff()).toBe(1);
+	});
+});
