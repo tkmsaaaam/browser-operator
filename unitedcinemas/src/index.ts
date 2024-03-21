2735dc6d7aa04524b34d04812eb21135f97f6e88
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-03-21 21:38:01 +0900

    add categories

--- unitedcinemas/src/index.ts
+++ unitedcinemas/src/index.ts
@@ -7,27 +7,38 @@ type Movie = {
 	title: string | null;
 };
 
+type Result = {
+	finishSoon: Movie[];
+	current: Movie[];
+	publishSoon: Movie[];
+};
+
 const logger = log4js.getLogger();
-logger.level = 'all';
+log4js.configure({
+	appenders: {
+		out: { type: 'stdout' },
+		file: {
+			type: 'file',
+			filename: 'output.txt',
+			layout: { type: 'pattern', pattern: '%m' },
+		},
+	},
+	categories: {
+		default: { appenders: ['out', 'file'], level: 'all' },
+	},
+});
 
 const jsdom = new JSDOM();
 const parser = new jsdom.window.DOMParser();
 
-(async () => {
-	const theater = process.env.THEATER;
-	if (!theater) {
-		logger.error(
-			'The theater is not designated. https://www.unitedcinemas.jp/index.html',
-		);
-		return;
-	}
-
+const getPublishSoonList = async (theater: string) => {
 	const url = 'https://www.unitedcinemas.jp/' + theater + '/movie.php';
 	const res = await fetch(url);
-  if (!res.ok) {
-    logger.error(`HTTP Request is failed. url: ${url} status: ${res.statusText}`)
-    return
-  }
+	if (!res.ok) {
+		return new Error(
+			`HTTP Request is failed. url: ${url} status: ${res.statusText}`,
+		);
+	}
 	const buf = await res.arrayBuffer();
 	const strhtml = new TextDecoder('shift-jis').decode(buf);
 
@@ -37,7 +48,7 @@ const parser = new jsdom.window.DOMParser();
 		.getElementsByClassName('movieList')[0]
 		.getElementsByTagName('li');
 
-	const result: Movie[] = Array.from(movieList)
+	return Array.from(movieList)
 		.filter(movie => movie.id)
 		.map(movie => {
 			const em = movie.getElementsByTagName('em')[0];
@@ -60,5 +71,83 @@ const parser = new jsdom.window.DOMParser();
 				title: a.textContent,
 			};
 		});
-	console.log('%o', result);
+};
+
+const getCurrent = async (theater: string) => {
+	const url = 'https://www.unitedcinemas.jp/' + theater + '/film.php';
+	const res = await fetch(url);
+	if (!res.ok) {
+		return new Error(
+			`HTTP Request is failed. url: ${url} status: ${res.statusText}`,
+		);
+	}
+	const buf = await res.arrayBuffer();
+	const strhtml = new TextDecoder('shift-jis').decode(buf);
+
+	const doc = parser.parseFromString(strhtml, 'text/html');
+
+	const movieList = doc.getElementsByClassName('movieList');
+
+	const currentList = movieList[0].getElementsByClassName('movieHead');
+
+	const current = Array.from(currentList).map(movie => {
+		const em = movie.getElementsByTagName('em');
+		let dateStr: string | null;
+		if (em.length > 0 && em[0].textContent) {
+			const start = em[0].textContent.indexOf('202');
+			if (start != -1) {
+				dateStr = em[0].textContent.substring(start, start + 10);
+			} else {
+				dateStr = null;
+			}
+		} else {
+			dateStr = null;
+		}
+		const a = movie.getElementsByTagName('a')[0];
+		return {
+			url: 'https://www.unitedcinemas.jp' + a.href,
+			dateStr: dateStr,
+			title: a.textContent,
+		};
+	});
+
+	const finishSoonList = movieList[1].getElementsByClassName('movieHead');
+
+	const finishSoon = Array.from(finishSoonList).map(movie => {
+		const a = movie.getElementsByTagName('a')[0];
+		return {
+			url: 'https://www.unitedcinemas.jp' + a.href,
+			dateStr: null,
+			title: a.textContent,
+		};
+	});
+	return { current, finishSoon };
+};
+
+(async () => {
+	const theater = process.env.THEATER;
+	if (!theater) {
+		logger.error(
+			'The THEATER is not designated. https://www.unitedcinemas.jp/index.html',
+		);
+		return;
+	}
+
+	const publishSoon = await getPublishSoonList(theater);
+	if (publishSoon instanceof Error) {
+		logger.error(publishSoon.message);
+		return;
+	}
+	const current = await getCurrent(theater);
+	if (current instanceof Error) {
+		logger.error(current.message);
+		return;
+	}
+	const result: Result = {
+		finishSoon: current.finishSoon,
+		current: current.current,
+		publishSoon: publishSoon,
+	};
+
+	logger.info('%o', result);
 })();
